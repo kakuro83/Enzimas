9e4e0201089926f5e7efcea47bc2c98052994be5
kakuro83 <kahicer@gmail.com>
2025-11-26 22:37:57 -0500

    Add Michaelis-Menten, Haldane, and Hill models

---
+++ modelos/un_sustrato.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def michaelis_menten(S, Vmax, Km):
+    """
+    Modelo de Michaelis-Menten.
+    S: Concentración de sustrato
+    Vmax: Velocidad máxima
+    Km: Constante de Michaelis
+    """
+    return (Vmax * S) / (Km + S)
+
+def haldane(S, Vmax, Km, Ki):
+    """
+    Modelo de Haldane (Inhibición por sustrato).
+    S: Concentración de sustrato
+    Vmax: Velocidad máxima
+    Km: Constante de saturación
+    Ki: Constante de inhibición
+    """
+    return (Vmax * S) / (Km + S + (S**2 / Ki))
+
+def hill(S, Vmax, K05, n):
+    """
+    Modelo de Hill (Cooperatividad).
+    S: Concentración de sustrato
+    Vmax: Velocidad máxima
+    K05: Constante de semi-saturación (equivalente a Km)
+    n: Coeficiente de Hill
+    """
+    return (Vmax * (S**n)) / (K05**n + S**n)
