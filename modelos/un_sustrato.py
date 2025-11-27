937c7a1ef5251406d6e80197e920f19beb3f62d3
kakuro83 <kahicer@gmail.com>
2025-11-27 00:23:32 -0500

    Update un_sustrato.py

--- modelos/un_sustrato.py
+++ modelos/un_sustrato.py
@@ -8,12 +8,12 @@ def michaelis_menten(S, Vmax, Km):
 
 def haldane(S, Vmax, Km, Ki):
     r"""
-    $V = \frac{V_{\max}\, S}{K_m + S + \frac{S^2}{K_i}}$
+    V = \frac{V_{\max}\, S}{K_m + S + \frac{S^2}{K_i}}
     """
     return (Vmax * S) / (Km + S + (S**2 / Ki))
 
 def hill(S, Vmax, K05, n):
     r"""
-    $V = \frac{V_{\max}\, S^n}{K_d^n + S^n}$
+    V = \frac{V_{\max}\, S^n}{K_d^n + S^n}
     """
     return (Vmax * (S**n)) / (K05**n + S**n)
