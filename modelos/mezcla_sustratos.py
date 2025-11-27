4994b6e359cdf966479ddd282cbab4b51bd8dfe1
kakuro83 <kahicer@gmail.com>
2025-11-27 00:35:33 -0500

    Rename K_half to Kh in hill_generalizado_mezcla

--- modelos/mezcla_sustratos.py
+++ modelos/mezcla_sustratos.py
@@ -1,16 +1,16 @@
 import numpy as np
 
-def hill_generalizado_mezcla(X, Vmax, K_half, n, beta):
+def hill_generalizado_mezcla(X, Vmax, Kh, n, beta):
     r"""
-    v = \frac{V_{\max}\, (S_1 + \beta S_2)^n}{K_{\mathrm{half}}^{\,n} + (S_1 + \beta S_2)^n}
+    V = \frac{V_{\max}\, (S_1 + \beta S_2)^n}{K_{\mathrm{h}}^{\,n} + (S_1 + \beta S_2)^n}
     """
     S1, S2 = X  # Desempaquetamos las variables
     
     # Ecuación suministrada:
-    # v = Vmax * (S1 + beta*S2)**n / (K_half**n + (S1 + beta*S2)**n)
+    # v = Vmax * (S1 + beta*S2)**n / (Kh**n + (S1 + beta*S2)**n)
     
     termino_comun = S1 + (beta * S2)
     numerador = Vmax * (termino_comun**n)
-    denominador = (K_half**n) + (termino_comun**n)
+    denominador = (Kh**n) + (termino_comun**n)
     
     return numerador / denominador
