b3a3cb783c0d18f34750471b958dbdbee29d8bf3
kakuro83 <kahicer@gmail.com>
2025-11-27 00:38:13 -0500

    Rename Km_A and Km_B to KA and KB

--- modelos/dos_sustratos.py
+++ modelos/dos_sustratos.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def ping_pong_bi_bi(X, Vmax, Km_A, Km_B):
+def ping_pong_bi_bi(X, Vmax, KA, KB):
     r"""
     v = \frac{V_{\max}\, A\, B}{K_A B + K_B A}
     """
@@ -8,6 +8,6 @@ def ping_pong_bi_bi(X, Vmax, Km_A, Km_B):
     
     # Ecuación de velocidad inicial para Ping-Pong Bi-Bi
     numerador = Vmax * S_A * S_B
-    denominador = (Km_B * S_A) + (Km_A * S_B) + (S_A * S_B)
+    denominador = (KB * S_A) + (KA * S_B) + (S_A * S_B)
     
     return numerador / denominador
