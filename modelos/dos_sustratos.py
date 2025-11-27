9363e5110377b99aadd6dff5f8d704cda8ce7f95
kakuro83 <kahicer@gmail.com>
2025-11-27 00:39:07 -0500

    Modify velocity equation in ping_pong_bi_bi function
    Updated the velocity equation to include additional terms.

--- modelos/dos_sustratos.py
+++ modelos/dos_sustratos.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def ping_pong_bi_bi(X, Vmax, KA, KB):
     r"""
-    v = \frac{V_{\max}\, A\, B}{K_A B + K_B A}
+    v = \frac{V_{\max}\, A\, B}{K_A B + K_B A + S_A S_B}
     """
     S_A, S_B = X # Desempaquetar
     
