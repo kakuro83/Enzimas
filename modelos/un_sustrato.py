dfab5c77bc40bb10da19470922547eee3d6a97c5
kakuro83 <kahicer@gmail.com>
2025-11-27 00:24:53 -0500

    Update haldane and hill functions parameters

--- modelos/un_sustrato.py
+++ modelos/un_sustrato.py
@@ -6,14 +6,14 @@ def michaelis_menten(S, Vmax, Km):
     """
     return (Vmax * S) / (Km + S)
 
-def haldane(S, Vmax, Km, Ki):
+def haldane(S, Vmax, Km, Ks):
     r"""
-    V = \frac{V_{\max}\, S}{K_m + S + \frac{S^2}{K_i}}
+    V = \frac{V_{\max}\, S}{K_m + S + \frac{S^2}{K_s}}
     """
-    return (Vmax * S) / (Km + S + (S**2 / Ki))
+    return (Vmax * S) / (Km + S + (S**2 / Ks))
 
-def hill(S, Vmax, K05, n):
+def hill(S, Vmax, Kd, n):
     r"""
     V = \frac{V_{\max}\, S^n}{K_d^n + S^n}
     """
-    return (Vmax * (S**n)) / (K05**n + S**n)
+    return (Vmax * (S**n)) / (Kd**n + S**n)
