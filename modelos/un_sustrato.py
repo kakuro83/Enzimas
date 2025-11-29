08d953cc05a530591230bc4ae9e800402f97d653
kakuro83 <kahicer@gmail.com>
2025-11-29 14:51:06 -0500

    Update un_sustrato.py

--- modelos/un_sustrato.py
+++ modelos/un_sustrato.py
@@ -19,24 +19,20 @@ def hill(S, Vmax, K05, n):
     """
     return (Vmax * (S**n)) / (K05**n + S**n)
 
-def Adair(a, Vmax, K1, K2, K3, K4):
+def adair_simplificado(S, Vmax, a, b, c, d):
     r"""
-    v = V_{\max} \cdot \frac{\frac{a}{K_1} + \frac{3a^2}{K_1 K_2} + \frac{3a^3}{K_1 K_2 K_3} + \frac{a^4}{K_1 K_2 K_3 K_4}}{1 + \frac{4a}{K_1} + \frac{6a^2}{K_1 K_2} + \frac{4a^3}{K_1 K_2 K_3} + \frac{a^4}{K_1 K_2 K_3 K_4}}
+    v = V_{\max} \frac{a S + 3 b S^2 + 3 c S^3 + d S^4}{1 + 4 a S + 6 b S^2 + 4 c S^3 + d S^4}
     """
-    # Términos individuales de la fracción (a / K...)
-    t1 = a / K1
-    t2 = (a**2) / (K1 * K2)
-    t3 = (a**3) / (K1 * K2 * K3)
-    t4 = (a**4) / (K1 * K2 * K3 * K4)
+    # Precalcular potencias de S
+    S2 = S**2
+    S3 = S**3
+    S4 = S**4
     
-    # Numerador según la imagen (Coeficientes: 1, 3, 3, 1)
-    numerador_y = t1 + (3 * t2) + (3 * t3) + t4
+    # Numerador: (a*S) + (3*b*S^2) + (3*c*S^3) + (d*S^4)
+    numerador = (a * S) + (3 * b * S2) + (3 * c * S3) + (d * S4)
     
-    # Denominador según la imagen (Coeficientes: 1, 4, 6, 4, 1)
-    denominador_y = 1 + (4 * t1) + (6 * t2) + (4 * t3) + t4
-    
-    # Fracción de saturación (y)
-    y = np.divide(numerador_y, denominador_y, out=np.zeros_like(numerador_y), where=denominador_y != 0)
+    # Denominador: 1 + (4*a*S) + (6*b*S^2) + (4*c*S^3) + (d*S^4)
+    denominador = 1 + (4 * a * S) + (6 * b * S2) + (4 * c * S3) + (d * S4)
     
     # Velocidad final
-    return Vmax * y
+    return np.divide(Vmax * numerador, denominador, out=np.zeros_like(numerador), where=denominador != 0)
