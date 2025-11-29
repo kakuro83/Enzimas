f26de3116bba29a0cb1b70e7e30539ff3b6c0de2
kakuro83 <kahicer@gmail.com>
2025-11-29 13:01:13 -0500

    Update dos_sustratos.py

--- modelos/dos_sustratos.py
+++ modelos/dos_sustratos.py
@@ -29,7 +29,7 @@ def hill_generalizado_mezcla(X, Vmax, Kh, n, beta):
 
 def inhibicion_mixta(X, Vmax, Km, Ki, Kip):
     r"""
-    v = \frac{V_{\max} S_1}{K_m \cdot (1 + \frac{I}{K_i \alpha}) + S_1 \cdot (1 + \frac{I}{K_i})}
+    v = \frac{V_{\max} S}{K_m \cdot (1 + \frac{I}{K_i \alpha}) + S \cdot (1 + \frac{I}{K_i'})}
     """
     # X debe ser una tupla o lista: [S1, S2]
     S1, S2 = X
@@ -38,6 +38,6 @@ def inhibicion_mixta(X, Vmax, Km, Ki, Kip):
     alpha_safe = np.where(alpha < 1e-6, 1e-6, alpha)
     
     numerador = Vmax * S1
-    denominador = Km * (1 + S2 / (Ki * alpha_safe)) + S1 * (1 + S2 / Ki)
+    denominador = Km * (1 + S2 / Ki) + S1 * (1 + S2 / Kip)
     
     return numerador / denominador
