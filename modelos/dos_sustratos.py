59cf04b3d75f34a9fcadab1ea96ce26cedf7e5c2
kakuro83 <kahicer@gmail.com>
2025-11-29 12:16:24 -0500

    Implement hill_generalizado_mezcla function
    Added a generalized Hill equation function for mixtures.

--- modelos/dos_sustratos.py
+++ modelos/dos_sustratos.py
@@ -11,3 +11,18 @@ def ping_pong_bi_bi(X, Vmax, KA, KB):
     denominador = (KB * S_A) + (KA * S_B) + (S_A * S_B)
     
     return numerador / denominador
+
+def hill_generalizado_mezcla(X, Vmax, Kh, n, beta):
+    r"""
+    V = \frac{V_{\max}\, (S_1 + \beta S_2)^n}{K_{\mathrm{h}}^{\,n} + (S_1 + \beta S_2)^n}
+    """
+    S1, S2 = X  # Desempaquetamos las variables
+    
+    # Ecuación suministrada:
+    # v = Vmax * (S1 + beta*S2)**n / (Kh**n + (S1 + beta*S2)**n)
+    
+    termino_comun = S1 + (beta * S2)
+    numerador = Vmax * (termino_comun**n)
+    denominador = (Kh**n) + (termino_comun**n)
+    
+    return numerador / denominador
